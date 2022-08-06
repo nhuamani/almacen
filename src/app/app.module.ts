52723883d8361a913d81b72fbc72d3f36709c9bf
nhuamani <nhuamani.c7@gmail.com>
2022-08-05 21:41:01 -0500

    Dashboard layout integration with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { AsideComponent } from './shared/aside/aside.component';
+import { MainComponent } from './shared/main/main.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    AsideComponent,
+    MainComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/aside/aside.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aside',
+  templateUrl: './aside.component.html'
+})
+export class AsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
